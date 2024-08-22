f0035c1a1935c3550dc3f34e88ebe2d6f2af5587
Chanwit <chanwit.jam@gmail.com>
2024-08-22 11:47:21 +0700

    fix path

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,6 +1,6 @@
 import  express from 'express'
 const router = express.Router()
-import  { getUsers,setUser,deleteUserByUsername } from '../controllers/userController.js'
+import  { getUsers,setUser,deleteUserByUsername } from '../controllers/UserController.js'
 
 
 router.route('/').get(getUsers).post(setUser).delete(deleteUserByUsername)
